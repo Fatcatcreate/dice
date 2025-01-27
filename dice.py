892b3808de19c23751a279338c3d4f2f70b855dd
Fatcatcreate <75983504+Fatcatcreate@users.noreply.github.com>
2025-01-27 21:20:10 +0000

    Update dice.py

--- dice.py
+++ dice.py
@@ -1,37 +1,33 @@
-fun=input('method?')
-numdice=int(input("how mnay dice"))
-l=[]
-r=0
-z=0
-j=0
+fun = input('method?')
+numdice = int(input("how many dice?"))
+l = []
+r = 0
+z = 0
+j = 0
 def repeat(x):
-  for i in range (x):
-    return i
-
-for i in range (numdice):
-  l.append(int(input('sides?')))
-
+    for i in range(x):
+        return i
+for i in range(numdice):
+    l.append(int(input('sides?')))
 for i in l:
-  suml=0
-  suml=i//2+0.5+suml
-  print(suml)
-  j=j+suml
+    suml = 0
+    suml = i // 2 + 0.5 + suml
+    print(suml)
+    j = j + suml
 print(j)
 for i in fun:
-  if i == D:
-    c=fun.index('D')
-    z=fun[c+1]  
-
+    if i == 'D':
+        c = fun.index('D')
+        z = int(fun[c + 1:])
 for i in l:
-  k=[]
-  b=[]
-  for i in range (len(l)):
-    p=repeat(i)
-    k.append(p)
-    k.sort()
-    q=len(k)
-    k.remove([z:q])
-    b.append(k.sum())
-  
-d=sum(b)
-avg=d/len(b)
+    k = []
+    b = []
+    for i in range(len(l)):
+        p = repeat(i)
+        k.append(p)
+        k.sort()
+        q = len(k)
+        k = k[z:q]
+        b.append(sum(k))
+d = sum(b)
+avg = d / len(b)
